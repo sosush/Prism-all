0f9dff75506810aec63b606b88b6c9ac6b4a42da
Sumit-2028 <sg287542@gmail.com>
2026-01-10 22:29:48 +0530

    Added chroma-main and security-core modules

--- security-core/src/main/java/com/prism/security_core/blockchain/BlockchainService.java
+++ security-core/src/main/java/com/prism/security_core/blockchain/BlockchainService.java
@@ -55,12 +55,12 @@ public class BlockchainService {
         return receipt.getTransactionHash();
     }
 
-    private String sha256(String input) throws Exception {
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        byte[] hash = md.digest(input.getBytes(StandardCharsets.UTF_8));
-        StringBuilder sb = new StringBuilder();
-        for (byte b : hash)
-            sb.append(String.format("%02x", b));
-        return sb.toString();
-    }
+    // private String sha256(String input) throws Exception {
+    //     MessageDigest md = MessageDigest.getInstance("SHA-256");
+    //     byte[] hash = md.digest(input.getBytes(StandardCharsets.UTF_8));
+    //     StringBuilder sb = new StringBuilder();
+    //     for (byte b : hash)
+    //         sb.append(String.format("%02x", b));
+    //     return sb.toString();
+    // }
 }
